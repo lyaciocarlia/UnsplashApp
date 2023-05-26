244629b17ebc1e66c1c5d5c59cae46225d45c86e
lyaciocarlia <lyass.media@gmail.com>
2023-05-26 16:54:18 +0300

    Create the UI for the login screen~

--- UnsplashApp/Authetification/Login/View/LoginViewController.swift
+++ UnsplashApp/Authetification/Login/View/LoginViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 import CoreImage
 
 class LoginViewController: UIViewController, LoginViewProtocol {
+<<<<<<< HEAD
     
     var viewModel: AuthenticationViewModel?
     var coordinator: AuthetificationCoordinatorProtocol?
@@ -41,12 +42,36 @@ extension LoginViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
+=======
+
+    var viewModel: LoginViewModelProtocol?
+    var coordinator: AuthetificationCoordinatorProtocol?
+    
+    @IBOutlet weak var fadedImage: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+            // Set the navigation bar to be transparent
+            navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+            navigationController?.navigationBar.shadowImage = UIImage()
+            navigationController?.navigationBar.isTranslucent = true
+       
+        let image = UIImage(named: "hossein-nasr-oVWhUno9nSw-unsplash 2")
+        fadedImage.image = applyGradientToImage(image: image ?? UIImage())
+
+    }
+    
+    @IBAction func login(_ sender: Any) {
+        viewModel?.authenticate(username: "", password: "")
+>>>>>>> d574f18 (Create the UI for the login screen~)
         viewModel?.isAuthenticated.bind { [weak self] isAuthenticated in
             if isAuthenticated {
                 self?.authenticationSuccessful()
             }
         }
     }
+<<<<<<< HEAD
 }
 
 // MARK: - UI SETUP
@@ -58,6 +83,19 @@ extension LoginViewController {
         let image = UIImage(named: "hossein-nasr-oVWhUno9nSw-unsplash 2")
         fadedImage.image = applyGradientToImage(image: image ?? UIImage())
         setupTextField(textField: emailTextField)
+=======
+    
+    @IBAction func createAcc(_ sender: Any) {
+        coordinator?.openCreateAccScreen()
+    }
+    
+    @IBAction func forgotPass(_ sender: Any) {
+        coordinator?.openForgotPasswordScreen()
+    }
+    
+    func authenticationSuccessful() {
+        coordinator?.finishAuthentication()
+>>>>>>> d574f18 (Create the UI for the login screen~)
     }
     
     func applyGradientToImage(image: UIImage) -> UIImage? {
@@ -69,25 +107,44 @@ extension LoginViewController {
             return nil
         }
         
+<<<<<<< HEAD
         image.draw(at: .zero)
         
         let colors = [UIColor(red: 255, green: 255, blue: 255, alpha: 0.5).cgColor, UIColor.white.cgColor]
         
+=======
+        // Draw the original image
+        image.draw(at: .zero)
+        
+        // Define gradient colors
+        let colors = [UIColor(red: 255, green: 255, blue: 255, alpha: 0.5).cgColor, UIColor.white.cgColor]
+        
+        // Create gradient parameters
+>>>>>>> d574f18 (Create the UI for the login screen~)
         let locations: [CGFloat] = [0, 1]
         guard let gradient = CGGradient(colorsSpace: CGColorSpaceCreateDeviceRGB(), colors: colors as CFArray, locations: locations) else {
             UIGraphicsEndImageContext()
             return nil
         }
         
+<<<<<<< HEAD
+=======
+        // Apply the gradient to the image
+>>>>>>> d574f18 (Create the UI for the login screen~)
         let startPoint = CGPoint(x: imageSize.width / 2, y: 0.0)
         let endPoint = CGPoint(x: imageSize.width / 2, y: imageSize.height)
         context.drawLinearGradient(gradient, start: startPoint, end: endPoint, options: [])
         
+<<<<<<< HEAD
+=======
+        // Get the resulting image from the context
+>>>>>>> d574f18 (Create the UI for the login screen~)
         let gradientImage = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
         
         return gradientImage
     }
+<<<<<<< HEAD
 }
 
 // MARK: - UITEXT FIELD SETUP
@@ -109,4 +166,9 @@ extension LoginViewController: UITextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         return true
     }
+=======
+
+
+    
+>>>>>>> d574f18 (Create the UI for the login screen~)
 }
