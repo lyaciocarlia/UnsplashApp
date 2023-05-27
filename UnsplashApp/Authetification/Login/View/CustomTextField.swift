29aaf52bc034e3593104f163d9a6fb280db775c2
lyaciocarlia <lyass.media@gmail.com>
2023-05-27 19:40:13 +0300

    Add login screen

---
+++ UnsplashApp/Authetification/Login/View/CustomTextField.swift
@@ -0,0 +1,23 @@
+//
+//  CustomTextField.swift
+//  UnsplashApp
+//
+//  Created by Iuliana Stecalovici  on 27.05.2023.
+//
+
+import Foundation
+import UIKit
+
+class CustomTextField: UITextField {
+    private let underlineColor: UIColor = .label
+       private let underlineWidth: CGFloat = 1.0
+
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        
+        let bottomLine = CALayer()
+        bottomLine.frame = CGRect(x: 0, y: frame.height - underlineWidth - 1, width: frame.width, height: underlineWidth)
+        bottomLine.backgroundColor = underlineColor.cgColor
+        layer.addSublayer(bottomLine)
+    }
+}
