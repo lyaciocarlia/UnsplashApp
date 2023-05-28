c0c59480165a7b2539c0c2dd2f3d64f07a82aa29
lyaciocarlia <lyass.media@gmail.com>
2023-05-28 14:23:32 +0300

    Use keychain

---
+++ UnsplashApp/Authetification/Authetification/ViewModel/AuthenticationViewModel.swift
@@ -0,0 +1,36 @@
+//
+//  AuthenticationViewModel.swift
+//  UnsplashApp
+//
+//  Created by Iuliana Stecalovici  on 25.05.2023.
+//
+
+import Foundation
+import UIKit
+
+class AuthenticationViewModel: AuthenticationViewModelProtocol {
+    
+    var isAuthenticated: Observable<Bool> = Observable(false)
+    
+    let service: AppServiceProtocol = AppService()
+    
+    func authenticate(email: String, password: String) {
+        if service.login(email: email, password: password)
+        {
+            self.isAuthenticated.value = true
+        }
+    }
+    
+    var createAccError: Observable<String?> = Observable(nil)
+    
+    func createAcc(email: String, password: String) {
+        let user = User(email: email, password: password)
+        let status = service.createAcc(user: user)
+        if status == nil {
+            self.isAuthenticated.value = true
+        } else {
+            self.createAccError.value = status!.localizedDescription
+        }
+    }
+
+}
