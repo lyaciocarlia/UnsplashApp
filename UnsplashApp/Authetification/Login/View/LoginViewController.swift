6223ec5d322d6734d756d11cd6af797302a4d0f6
lyaciocarlia <lyass.media@gmail.com>
2023-05-28 14:23:32 +0300

    Use keychain

--- UnsplashApp/Authetification/Login/View/LoginViewController.swift
+++ UnsplashApp/Authetification/Login/View/LoginViewController.swift
@@ -10,6 +10,7 @@ import CoreImage
 
 class LoginViewController: UIViewController, LoginViewProtocol {
 <<<<<<< HEAD
+<<<<<<< HEAD
     
     var viewModel: AuthenticationViewModel?
     var coordinator: AuthetificationCoordinatorProtocol?
@@ -45,26 +46,45 @@ extension LoginViewController {
 =======
 
     var viewModel: LoginViewModelProtocol?
+=======
+    
+    var viewModel: AuthenticationViewModel?
+>>>>>>> 85d9b76 (Use keychain)
     var coordinator: AuthetificationCoordinatorProtocol?
     
     @IBOutlet weak var fadedImage: UIImageView!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-            // Set the navigation bar to be transparent
-            navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
-            navigationController?.navigationBar.shadowImage = UIImage()
-            navigationController?.navigationBar.isTranslucent = true
-       
-        let image = UIImage(named: "hossein-nasr-oVWhUno9nSw-unsplash 2")
-        fadedImage.image = applyGradientToImage(image: image ?? UIImage())
-
-    }
+    @IBOutlet weak var emailTextField: CustomTextField!
+    @IBOutlet weak var passwordTextField: CustomTextField!
     
     @IBAction func login(_ sender: Any) {
+<<<<<<< HEAD
         viewModel?.authenticate(username: "", password: "")
 >>>>>>> d574f18 (Create the UI for the login screen~)
+=======
+        authenticate()
+    }
+    
+    @IBAction func openCreateAccScreen(_ sender: Any) {
+        coordinator?.openCreateAccScreen()
+    }
+    
+    @IBAction func openForgotPassScreen(_ sender: Any) {
+        coordinator?.openForgotPasswordScreen()
+    }
+    
+    func authenticationSuccessful() {
+        coordinator?.finishAuthentication()
+    }
+    
+}
+
+// MARK: - LIFE CYCLE
+
+extension LoginViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+>>>>>>> 85d9b76 (Use keychain)
         viewModel?.isAuthenticated.bind { [weak self] isAuthenticated in
             if isAuthenticated {
                 self?.authenticationSuccessful()
@@ -72,11 +92,15 @@ extension LoginViewController {
         }
     }
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 85d9b76 (Use keychain)
 }
 
 // MARK: - UI SETUP
 
 extension LoginViewController {
+<<<<<<< HEAD
     
     func setupUI() {
         navigationController?.navigationBar.isTranslucent = true
@@ -96,6 +120,14 @@ extension LoginViewController {
     func authenticationSuccessful() {
         coordinator?.finishAuthentication()
 >>>>>>> d574f18 (Create the UI for the login screen~)
+=======
+    
+    func setupUI() {
+        navigationController?.navigationBar.isTranslucent = true
+        let image = UIImage(named: "hossein-nasr-oVWhUno9nSw-unsplash 2")
+        fadedImage.image = applyGradientToImage(image: image ?? UIImage())
+        setupTextField(textField: emailTextField)
+>>>>>>> 85d9b76 (Use keychain)
     }
     
     func applyGradientToImage(image: UIImage) -> UIImage? {
@@ -108,19 +140,24 @@ extension LoginViewController {
         }
         
 <<<<<<< HEAD
+<<<<<<< HEAD
         image.draw(at: .zero)
         
         let colors = [UIColor(red: 255, green: 255, blue: 255, alpha: 0.5).cgColor, UIColor.white.cgColor]
         
 =======
         // Draw the original image
+=======
+>>>>>>> 85d9b76 (Use keychain)
         image.draw(at: .zero)
         
-        // Define gradient colors
         let colors = [UIColor(red: 255, green: 255, blue: 255, alpha: 0.5).cgColor, UIColor.white.cgColor]
         
+<<<<<<< HEAD
         // Create gradient parameters
 >>>>>>> d574f18 (Create the UI for the login screen~)
+=======
+>>>>>>> 85d9b76 (Use keychain)
         let locations: [CGFloat] = [0, 1]
         guard let gradient = CGGradient(colorsSpace: CGColorSpaceCreateDeviceRGB(), colors: colors as CFArray, locations: locations) else {
             UIGraphicsEndImageContext()
@@ -128,23 +165,32 @@ extension LoginViewController {
         }
         
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
         // Apply the gradient to the image
 >>>>>>> d574f18 (Create the UI for the login screen~)
+=======
+>>>>>>> 85d9b76 (Use keychain)
         let startPoint = CGPoint(x: imageSize.width / 2, y: 0.0)
         let endPoint = CGPoint(x: imageSize.width / 2, y: imageSize.height)
         context.drawLinearGradient(gradient, start: startPoint, end: endPoint, options: [])
         
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
         // Get the resulting image from the context
 >>>>>>> d574f18 (Create the UI for the login screen~)
+=======
+>>>>>>> 85d9b76 (Use keychain)
         let gradientImage = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
         
         return gradientImage
     }
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 85d9b76 (Use keychain)
 }
 
 // MARK: - UITEXT FIELD SETUP
@@ -166,9 +212,12 @@ extension LoginViewController: UITextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         return true
     }
+<<<<<<< HEAD
 =======
 
 
     
 >>>>>>> d574f18 (Create the UI for the login screen~)
+=======
+>>>>>>> 85d9b76 (Use keychain)
 }
