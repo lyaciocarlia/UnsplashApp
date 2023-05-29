e989baeaa5e4258ba4f5fd2ca7149dac4ce98517
lyaciocarlia <lyass.media@gmail.com>
2023-05-29 12:03:17 +0300

    Fix the bugs after rebase

--- UnsplashApp/Authenticatio/Login/View/LoginViewController.swift
+++ UnsplashApp/Authenticatio/Login/View/LoginViewController.swift
@@ -8,9 +8,13 @@
 import UIKit
 
 class LoginViewController: UIViewController, LoginViewProtocol {
-
+    
     var viewModel: LoginViewModelProtocol
     var coordinator: AuthenticationCoordinatorProtocol
+    
+    @IBOutlet weak var fadedImage: UIImageView!
+    @IBOutlet weak var emailTextField: CustomTextField!
+    @IBOutlet weak var passwordTextField: CustomTextField!
     
     init(viewModel: LoginViewModelProtocol, coordinator: AuthenticationCoordinatorProtocol) {
         self.viewModel = viewModel
@@ -22,24 +26,15 @@ class LoginViewController: UIViewController, LoginViewProtocol {
         fatalError("init(coder:) has not been implemented")
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        viewModel.isAuthenticated.bind { [weak self] isAuthenticated in
-            if isAuthenticated {
-                self?.authenticationSuccessful()
-            }
-        }
-    }
-    
     @IBAction func login(_ sender: Any) {
-        viewModel.authenticate(username: "", password: "")
+        authenticate()
     }
     
-    @IBAction func createAcc(_ sender: Any) {
+    @IBAction func openCreateAccScreen(_ sender: Any) {
         coordinator.openCreateAccScreen()
     }
     
-    @IBAction func forgotPass(_ sender: Any) {
+    @IBAction func openForgotPassScreen(_ sender: Any) {
         coordinator.openForgotPasswordScreen()
     }
     
@@ -48,3 +43,80 @@ class LoginViewController: UIViewController, LoginViewProtocol {
     }
     
 }
+
+// MARK: - LIFE CYCLE
+
+extension LoginViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+        viewModel.isAuthenticated.bind { [weak self] isAuthenticated in
+            if isAuthenticated {
+                self?.authenticationSuccessful()
+            }
+        }
+    }
+}
+
+// MARK: - UI SETUP
+
+extension LoginViewController {
+    
+    func setupUI() {
+        navigationController?.navigationBar.isTranslucent = true
+        let image = UIImage(named: "hossein-nasr-oVWhUno9nSw-unsplash 2")
+        fadedImage.image = applyGradientToImage(image: image ?? UIImage())
+        setupTextField(textField: emailTextField)
+    }
+    
+    func applyGradientToImage(image: UIImage) -> UIImage? {
+        
+        let imageSize = fadedImage.frame.size
+        UIGraphicsBeginImageContextWithOptions(imageSize, false, 0.0)
+        
+        guard let context = UIGraphicsGetCurrentContext() else {
+            return nil
+        }
+        
+        image.draw(at: .zero)
+        
+        let colors = [UIColor(red: 255, green: 255, blue: 255, alpha: 0.5).cgColor, UIColor.white.cgColor]
+        
+        let locations: [CGFloat] = [0, 1]
+        guard let gradient = CGGradient(colorsSpace: CGColorSpaceCreateDeviceRGB(), colors: colors as CFArray, locations: locations) else {
+            UIGraphicsEndImageContext()
+            return nil
+        }
+        
+        let startPoint = CGPoint(x: imageSize.width / 2, y: 0.0)
+        let endPoint = CGPoint(x: imageSize.width / 2, y: imageSize.height)
+        context.drawLinearGradient(gradient, start: startPoint, end: endPoint, options: [])
+        
+        let gradientImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return gradientImage
+    }
+}
+
+// MARK: - UITEXT FIELD SETUP
+
+extension LoginViewController: UITextFieldDelegate {
+    
+    private func authenticate() {
+        passwordTextField.resignFirstResponder()
+        viewModel.authenticate(email: emailTextField.text ?? "", password: passwordTextField.text ?? "" )
+    }
+    
+    func setupTextField(textField: UITextField) {
+        textField.returnKeyType = .done
+        textField.textColor = .label
+        textField.autocorrectionType = .no
+        textField.becomeFirstResponder()
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        return true
+    }
+}
+
