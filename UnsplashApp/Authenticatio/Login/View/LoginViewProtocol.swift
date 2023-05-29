503955680440a94998587faaa79fe224d60ada56
lyaciocarlia <lyass.media@gmail.com>
2023-05-29 11:45:06 +0300

    Update after rebase

--- UnsplashApp/Authenticatio/Login/View/LoginViewProtocol.swift
+++ UnsplashApp/Authenticatio/Login/View/LoginViewProtocol.swift
@@ -9,11 +9,6 @@ import Foundation
 import UIKit
 
 protocol LoginViewProtocol: UIViewController{
-<<<<<<< HEAD:UnsplashApp/Authenticatio/Login/View/LoginViewProtocol.swift
         var viewModel: LoginViewModelProtocol { get set }
         var coordinator: AuthenticationCoordinatorProtocol { get set }
-=======
-        var viewModel: AuthenticationViewModel? { get set }
-        var coordinator: AuthetificationCoordinatorProtocol? { get set }
->>>>>>> ba307ff (Use keychain):UnsplashApp/Authetification/Login/View/LoginViewProtocol.swift
 }
