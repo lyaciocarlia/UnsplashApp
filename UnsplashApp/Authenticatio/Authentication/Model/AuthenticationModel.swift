a6af0b63d850c1e35ba235611d9bbadca964bafb
lyaciocarlia <lyass.media@gmail.com>
2023-06-02 15:00:48 +0300

    Fix the duplicates

--- UnsplashApp/Authenticatio/Authentication/Model/AuthenticationModel.swift
+++ UnsplashApp/Authenticatio/Authentication/Model/AuthenticationModel.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct User: UserProtocol {
+struct User {
     var email: String
     var password: String
 }
--- UnsplashApp/Authenticatio/Authentication/Model/AuthenticationModelProtocol.swift
+++ UnsplashApp/Authenticatio/Authentication/Model/AuthenticationModelProtocol.swift
@@ -6,8 +6,3 @@
 //
 
 import Foundation
-
-protocol UserProtocol {
-    var email: String { get set }
-    var password: String { get set }
-}
